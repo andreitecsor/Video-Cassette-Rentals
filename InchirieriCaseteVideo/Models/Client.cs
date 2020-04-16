92818ae9af700f05ed15709fc900157e434277d7
AndreiT <andrei.tecsor@gmail.com>
2020-04-17 02:42:05 +0300

    Clasele Client si Film completate

---
+++ InchirieriCaseteVideo/Models/Client.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InchirieriCaseteVideo.Models
+{
+    class Client: ICloneable, IComparable<Client>
+    {
+        #region Atribute si Proprietati Client
+        private int idClient_PK = 0;
+        public int IdClient { get; }
+
+        private string _nume;
+        public string Nume { get; set; }
+
+        private string _prenume;
+        public string Prenume { get; set; }
+
+        public DateTime DataNastere { get; set; }
+
+        public string Email { get; set; }
+
+        public string Telefon { get; set; }
+        #endregion
+
+        #region Constructori
+        public Client(string nume, string prenume, DateTime dataNastere, string email, string telefon)
+        {
+            _nume = nume;
+            _prenume = prenume;
+            DataNastere = dataNastere;
+            Email = email;
+            Telefon = telefon;
+            idClient_PK++;
+        }
+        #endregion
+
+        #region metode IComparable, IClonable
+        public int CompareTo(Client other)
+        {
+            return this.Email.CompareTo(other.Email);
+        }
+
+        public object Clone()
+        {
+            var clone = new Client(this.Nume, this.Prenume, this.DataNastere,this.Email,this.Telefon);
+            return clone;
+        }
+        #endregion
+    }
+}
---
+++ InchirieriCaseteVideo/Models/Film.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InchirieriCaseteVideo.Models
+{
+    class Film: ICloneable, IComparable<Film>
+    {
+        #region Atribute si Proprietati Film
+        private int idFilm_PK = 0;
+        public int IdFilm { get; }
+
+        private string _titlu;
+        public string Titlu {
+            get { return _titlu; }
+            set { _titlu = value; }
+        }
+
+        private double _pretPeZi;
+        public double PretPeZi
+        {
+            get { return _pretPeZi; }
+            set { _pretPeZi = value; }
+        }
+
+        private int _anAparitie;
+        public int AnAparitie
+        {
+            get { return _anAparitie; }
+            set { _anAparitie = value; }
+        }
+
+        private int _stoc;
+        public int Stoc
+        {
+            get { return _stoc; }
+            set { _stoc = value; }
+        }
+
+        public EnumGenFilm GenFilm { get; set; }
+        #endregion
+
+        #region Constructori
+        private Film()
+        {
+            _titlu = "Necompletat";
+            _pretPeZi = 0.0;
+            _anAparitie = 0;
+            _stoc = 0;
+        }
+
+        public Film(string titlu, double pret, int an, EnumGenFilm gen, int stoc)
+        {
+            _titlu = titlu;
+            if(pret > 0 )
+                _pretPeZi = pret;
+            if (an > 1900 && an < 2020)
+                _anAparitie = an;
+            if (stoc >= 0)
+                _stoc = stoc;
+            GenFilm = gen;
+            idFilm_PK++;
+        }
+        #endregion
+
+        #region metode IComparable, IClonable
+        public int CompareTo(Film other)
+        {
+            return this.Titlu.CompareTo(other.Titlu);
+        }
+
+        public object Clone()
+        {
+            var clone = new Film();
+            clone.Titlu = this.Titlu;
+            clone.PretPeZi = this.PretPeZi;
+            clone.AnAparitie = this.AnAparitie;
+            clone.GenFilm = this.GenFilm;
+            return clone;
+        }
+        #endregion
+
+    }
+}
